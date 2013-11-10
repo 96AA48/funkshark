26a492d24b5e48441a3b1c8f67f20939b7a8994e
Bram van der Veen <96aa48@gmail.com>
2013-11-10 01:10:26 +0100

    Made new build script.

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ zip u app.nw * -r -tzip
 zip u app.nw */* -r -tzip
 clear
 
-E:/Development/Libraries/node-webkit/nw.exe --enable-logging app.nw
+D:/Development/Libraries/node-webkit/nw.exe --enable-logging app.nw
